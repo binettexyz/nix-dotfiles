b45155b5fc8719f44186d14b23d21a2dee61f762
Binettexyz <46168797+Binettexyz@users.noreply.github.com>
2022-05-04 11:24:04 -0400

    update desktop config

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -4,10 +4,13 @@
 
       # enable flatpack
     services.flatpak.enable = true;
-    xdg.portal.enable = true;
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    };
 
       # enable steam (doesn't work)
-#    programs.steam.enable = true;
-#    hardware.steam-hardware.enable = true;
+    programs.steam.enable = true;
+    hardware.steam-hardware.enable = true;
 
   }
