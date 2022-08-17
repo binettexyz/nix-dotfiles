13858771c7e8ee916ab0df8f0efc2d64e665d4ca
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-08-17 11:47:13 -0400

    .

--- services/net/ssh.nix
+++ services/net/ssh.nix
@@ -3,18 +3,16 @@
   services.openssh = {
     enable = true;
     startWhenNeeded = true;
-    passwordAuthentication = true;
-    allowSFTP = false; # Don't set this if you need sftp
+    passwordAuthentication = false;
+#    allowSFTP = false; # Don't set this if you need sftp
     kbdInteractiveAuthentication = false;
-    # nixos 22.05
-#    kbdInteractiveAuthentication = false;
     forwardX11 = false;
     extraConfig = ''
       AllowTcpForwarding yes
       AllowAgentForwarding no
       AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
     '';
-    #AuthenticationMethods publickey
   };
 
 }
