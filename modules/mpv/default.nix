78ca3008c912523b2f31cfea4f48f650cfdf14c3
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-08-21 19:24:53 -0400

    .

---
+++ modules/mpv/default.nix
@@ -0,0 +1,26 @@
+#!/bin/nix
+{ pkgs, ... }: {
+  
+  
+  home-manager.users.binette.programs.mpv = {
+    enable = true;
+
+    bindings = {
+
+    };
+
+    config = {
+
+    };
+
+    profiles = {
+
+    };
+
+    scripts = {
+
+    };
+
+  };
+
+}
