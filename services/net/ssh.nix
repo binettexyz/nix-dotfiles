e947aa7adc37390911940736afa597f05b961da8
Binettexyz <46168797+Binettexyz@users.noreply.github.com>
2022-08-21 00:24:07 -0400

    .

--- services/net/ssh.nix
+++ services/net/ssh.nix
@@ -3,7 +3,7 @@
   services.openssh = {
     enable = true;
     startWhenNeeded = true;
-    passwordAuthentication = true;
+    passwordAuthentication = false;
 #    allowSFTP = false; # Don't set this if you need sftp
     kbdInteractiveAuthentication = false;
     forwardX11 = false;
