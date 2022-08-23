1975f9215ca896fd6e968bf81a6dfa8861e36ad2
Binettexyz <46168797+Binettexyz@users.noreply.github.com>
2022-08-23 18:09:13 -0400

    .

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -4,7 +4,7 @@
 #		./torrents/prowlarr.nix
 		./torrents/sonarr.nix
 		./torrents/radarr.nix
-#		./torrents/jackett.nix
+		./torrents/jackett.nix
 #		./torrents/plex.nix
 #		./torrents/deluge.nix
 		./torrents/bazarr.nix
