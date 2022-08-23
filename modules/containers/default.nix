ca369a4c6f446f59bf790d2ee0acbcf5b145a443
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-08-23 18:04:46 -0400

    .

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -1,10 +1,10 @@
 {
 
 	imports = [
-		./torrents/prowlarr.nix
+#		./torrents/prowlarr.nix
 		./torrents/sonarr.nix
 		./torrents/radarr.nix
-		./torrents/jackett.nix
+#		./torrents/jackett.nix
 #		./torrents/plex.nix
 #		./torrents/deluge.nix
 		./torrents/bazarr.nix
