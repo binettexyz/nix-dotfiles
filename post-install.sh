383bd1c81e6dcb21547f83d38b295b88d88f466f
binettexyz <46168797+binettexyz@users.noreply.github.com>
2022-09-10 22:04:15 -0400

    .

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,13 @@ set -e
 
 # Replace with your device
 dev=$1
+lvm=lvm
+
 crypt=off
+home=off
+
+  # Cloning NixOS Config
+git clone https://github.com/binettexyz/nix-dotfiles
 
   ### Partitioning ###
 
@@ -18,21 +24,21 @@ parted -s ${dev} name 1 nix
 if [ $crypt == "on" ]; then
       # Setup the encrypted LUKS partition and open it:
     cryptsetup luksFormat ${dev}2
-    cryptsetup open --type luks ${dev}2 lvm
+    cryptsetup open --type luks ${dev}2 $lvm
 fi
 
   # Create two logical volumes
-pvcreate ${dev}2
+pvcreate -ff ${dev}2
 vgcreate lvm ${dev}2
 
-lvcreate -L 16G lvm -n swap
-lvcreate -l 100%FREE lvm -n nix
+lvcreate -L 16G $lvm -n swap
+lvcreate -l 100%FREE $lvm -n nix
 
   # Format the partitions
 mkfs.vfat -n boot ${dev}1
-mkfs.ext4 -L nix /dev/lvm/nix
-mkswap -L swap /dev/lvm/swap
-swapon /dev/lvm/swap
+mkfs.ext4 -L nix /dev/$lvm/nix
+mkswap -L swap /dev/$lvm/swap
+swapon /dev/$lvm/swap
 
 
   ### Installing NixOS ###
@@ -41,30 +47,15 @@ swapon /dev/lvm/swap
 mount -t tmpfs none /mnt
 mkdir -p /mnt/{boot,home,nix,etc/{nixos,ssh},var/{lib,log},srv,tmp}
 mount ${dev}1 /mnt/boot
-mount /dev/lvm/nix /mnt/nix
+mount /dev/$lvm/nix /mnt/nix
   # Uncomment if it's a fresh install
 mkdir -p /mnt/nix/persist/{home,media,mounts,nix,etc/{nixos,ssh},var/{lib,log},root,srv}
 
 mount -o bind /mnt/nix/persist/etc/nixos /mnt/etc/nixos
 mount -o bind /mnt/nix/persist/var/log /mnt/var/log
 
-#  # Configure WPA_Supplicant for WIFI
-#echo "
-#network={
-#        ssid="Hal"
-#        psk=af8dca01536bdf1b08911c118df5971defa78264c21a376fbc41e92f628b6a26
-#}" >> /etc/wpa_supplicant
-#systemctl start wpa_supplicant
-
-  # Updating nix-channel
-nix-channel --add "https://github.com/NixOS/nixpkgs/archive/master.tar.gz" nixos
-nix-channel --add "https://github.com/nix-community/impermanence/archive/master.tar.gz" impermanence
-nix-channel --update
-
   # Copying NixOS Configs where it's supposed to be
-cp -R /home/root/nix-dotfiles/* /mnt/nix/persist/etc/nixos/.
+cp -R /home/nixos/nix-dotfiles/* /mnt/nix/persist/etc/nixos/.
 
-  # Install NixOS
+  # Set tmpfile into /mnt to prevent "not enough space" error
 export TMPDIR=/mnt/tmp
-
-#nixos-install
