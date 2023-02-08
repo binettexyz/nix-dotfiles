6bac588e97bf1343a70005f962753c1615e35321
binettexyz <binettexyz@proton.me>
2023-02-08 13:52:14 -0500

    Desktop: update host config

---
+++ hosts/desktop/config.nix
@@ -0,0 +1,66 @@
+{ config, flake, lib, pkgs, system, ... }: 
+  {
+
+    imports = [ 
+#      ../../modules/default.nix
+      ./hardware.nix
+      ../../modules/system/default.nix
+#      ../../nixos/system/default.nix
+    ];
+
+#  modules = {
+#    bootloader = "grub";
+#  };
+
+  ## Custom modules ##
+  modules = {
+    bootloader = "grub";
+    windowManager = "dwm";
+    transmission.enable = false;
+    services = {
+      greenclip.enable = true;
+      tty-login-prompt.enable = true;
+    };
+    profiles = {
+      gaming.enable = true;
+      core = {
+        enable = true;
+        bluetooth.enable = true;
+        wifi.enable = true;
+        print.enable = true;
+        ssd.enable = true;
+        virtmanager.enable = false;
+      };
+    };
+  };
+
+#    device = {
+#      type = "desktop";
+#      gpu = "nvidia";
+#      netDevices = [ "enp34s0" ];
+#    };
+  
+      # GPU
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware = {
+      nvidia = {
+        modesetting.enable = true;
+          # Enable experimental NVIDIA power management via systemd
+        powerManagement.enable = true;
+      };
+      opengl.extraPackages = with pkgs; [ vaapiVdpau ];
+    };
+  
+    ## Networking ##
+    networking = {
+      interfaces.wlo1.useDHCP = true;
+      interfaces.enp34s0.useDHCP = true;
+      interfaces.tailscale0.useDHCP = true;
+    };
+    
+    nix.settings.max-jobs = 16; # ryzen 7 5800x
+    hardware.cpu.amd.updateMicrocode = true;
+    
+    nixpkgs.config.allowUnfree = true;
+
+}
---
+++ hosts/desktop/hardware.nix
@@ -0,0 +1,85 @@
+{ config, flake, lib, modulesPath, pkgs, system, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    #inputs.hardware.nixosModules.common-cpu-intel
+  ];
+
+  boot = {
+    extraModulePackages = [ ];
+    kernelModules = [ "kvm-amd" "nvidia" ];
+#    kernelPackages =  pkgs.linuxPackages_zen;
+    kernelPackages =  pkgs.linuxPackages_xanmod;
+    kernelParams = [ /*"mitigations=off"*/ ];
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" /*"nvme"*/ "usbhid" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+    };
+  };
+
+  /* ---FileSystem--- */
+  fileSystems = {
+    "/" = { 
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=2G" "mode=755" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+    "/nix" = {
+      device = "/dev/disk/by-label/nix";
+      fsType = "ext4";
+    };
+    "/nix/persist/home" = {
+      device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+    };
+    "/mounts/nas" = {
+      device = "100.71.254.90:/media";
+      fsType = "nfs";
+        # don't freeze system if mount point not available on boot
+      options = [ "x-systemd.automount" "noauto" ];
+    };
+  };
+
+  swapDevices = [ /*{ device = "/swap"; size = 1024 * 8; options = [ "mode=600"]; }*/ ];
+
+  environment.persistence = {
+    "/nix/persist" = {
+      hideMounts = true;
+      directories = [
+        "/etc/nixos"
+        "/var/lib"
+        "/var/log"
+        "/mounts"
+        "/root"
+        "/srv"
+      ];
+    };
+    "/nix/persist/home/binette/.local/share" = {
+      hideMounts = true;
+      directories = [
+        "/opt"
+      ];
+    };
+  };
+
+  /* ---Screen resolution--- */
+  services.xserver = {
+    xrandrHeads = [{
+      output = "DP-2";
+      primary = true;
+      monitorConfig = ''
+          # 2560x1440 164.90 Hz (CVT) hsync: 261.86 kHz; pclk: 938.50 MHz
+        Modeline "2560x1440_165.00"  938.50  2560 2792 3072 3584  1440 1443 1448 1588 -hsync +vsync
+        Option "PreferredMode" "2560x1440_165.00"
+        Option "Position" "0 0"
+      '';
+    }];
+  };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave" /*"conservative"*/ ;
+}
