5661a7db3b5bfbff81abdd47b119dbc20d947966
Jonathan Binette <binettexyz@proton.me>
2023-03-02 13:56:54 -0500

    Git: add delta

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -5,9 +5,63 @@ with lib;
 
     programs.git = {
       enable = true;
-      userName = "binettexyz";
+      package = pkgs.gitFull;
+
+      userName = "Jonathan Binette";
       userEmail = "binettexyz@proton.me";
+
+    delta = {
+      enable = true;
+      options = {
+        features = "line-numbers decorations";
+        syntax-theme = "ansi";
+#        plus-style = ''syntax "#003800"'';
+#        minus-style = ''syntax "#3f0001"'';
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-style = "bold yellow ul";
+          file-decoration-style = "none";
+          hunk-header-decoration-style = "cyan box ul";
+        };
+        delta = {
+          navigate = true;
+        };
+        line-numbers = {
+          line-numbers-left-style = "cyan";
+          line-numbers-right-style = "cyan";
+          line-numbers-minus-style = 124;
+          line-numbers-plus-style = 28;
+        };
+      };
+    };
+
       aliases = {
+      };
+
+      ignores = [
+      ];
+
+      extraConfig = {
+        init = { defaultBranch = "master"; };
+        branch = { sort = "-committerdate"; };
+        color = { ui = true; };
+        commit = { verbose = true; };
+        core = {
+          editor = "nvim";
+          #whitespace = "trailing-space,space-before-tab,indent-with-non-tab";
+        };
+        checkout = { defaultRemote = "origin"; };
+        github = { user = "binettexyz"; };
+        merge = {
+          conflictstyle = "zdiff3";
+          tool = "nvim -d";
+        };
+        pull = { rebase = true; };
+        push = {
+          autoSetupRemote = true;
+          default = "simple";
+        };
+        rebase = { autoStash = true; };
       };
     };
 
