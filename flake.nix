eb9e99afa9787452e42dc11ccfde4c58ad62eae6
Jonathan Binette <binettexyz@proton.me>
2023-03-07 14:25:58 -0500

    Flake: update config

--- flake.nix
+++ flake.nix
@@ -28,16 +28,16 @@
     slstatus-desktop = { url = "github:binettexyz/slstatus/desktop"; flake = false; };
     dmenu = { url = "github:binettexyz/dmenu"; flake = false; };
 
-      /* --- Discord stuff --- */
-#    powercord-overlay.url = "github:LavaDesu/powercord-overlay";
-#    disc-betterReplies = { url = "github:cyyynthia/better-replies"; flake = false; };
-#    disc-doubleClickVC = { url = "github:discord-modifications/double-click-vc"; flake = false; };
-#    disc-muteNewGuild = { url = "github:RazerMoon/muteNewGuild"; flake = false; };
-#    disc-popoutFix = { url = "github:Nexure/PowerCord-Popout-Fix"; flake = false; };
-#    disc-screenshareCrack = { url = "github:discord-modifications/screenshare-crack"; flake = false; };
-#    disc-unindent = { url = "github:VenPlugs/Unindent"; flake = false; };
-#    disc-silentTyping = { url = "github:svby/powercord-silenttyping"; flake = false; };
-#    disc-gruvbox = { url = "github:binettexyz/discord-gruvbox"; flake = false; };
+    /* --- Discord stuff --- */
+    #powercord-overlay.url = "github:LavaDesu/powercord-overlay";
+    #disc-betterReplies = { url = "github:cyyynthia/better-replies"; flake = false; };
+    #disc-doubleClickVC = { url = "github:discord-modifications/double-click-vc"; flake = false; };
+    #disc-muteNewGuild = { url = "github:RazerMoon/muteNewGuild"; flake = false; };
+    #disc-popoutFix = { url = "github:Nexure/PowerCord-Popout-Fix"; flake = false; };
+    #disc-screenshareCrack = { url = "github:discord-modifications/screenshare-crack"; flake = false; };
+    #disc-unindent = { url = "github:VenPlugs/Unindent"; flake = false; };
+    #disc-silentTyping = { url = "github:svby/powercord-silenttyping"; flake = false; };
+    #disc-gruvbox = { url = "github:binettexyz/discord-gruvbox"; flake = false; };
 
     /* --- Minimize duplicate instances of inputs --- */
     home.inputs.nixpkgs.follows = "unstable";
@@ -71,7 +71,7 @@
           (./. + "/hosts/${hostname}/hardware.nix")
           ./shared/adblock.nix
           (import ./overlays { inherit inputs lib nixpkgs system pkgs unstable; }) 
-      
+
           inputs.sops-nix.nixosModules.sops
           inputs.nix-gaming.nixosModules.pipewireLowLatency
           inputs.impermanence.nixosModules.impermanence 
@@ -83,8 +83,8 @@
               users.binette = (./. + "/hosts/${hostname}/user.nix");
             };
             nixpkgs.overlays = [
-      #        powercord-overlay.overlay
-      #        nur.overlay
+              #powercord-overlay.overlay
+              #nur.overlay
             ];
           }
         ];
@@ -93,13 +93,9 @@
 
     /* ---Defining Systems--- */
     nixosConfigurations = {
-        # Workstation
       desktop = mkSystem inputs.unstable "x86_64-linux"  "desktop";
-        # Portable Laptop
       x240 = mkSystem inputs.unstable    "x86_64-linux"  "x240";
-        # Desktop Laptop
       t440p = mkSystem inputs.unstable   "x86_64-linux"  "t440p";
-        # Server
       rpi4 = mkSystem inputs.unstable    "aarch64-linux" "rpi4";
     };
   }; 
