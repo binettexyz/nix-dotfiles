ee0660ff16f5cf7846789750decea262a9c50bbd
Jonathan Binette <binettexyz@proton.me>
2023-03-10 14:21:26 -0500

    add catppuccin colorscheme

--- modules/colorSchemes/catppuccin.nix
+++ modules/colorSchemes/catppuccin.nix
@@ -1,1 +1,25 @@
-#TODO: Pitch black catppuccin theme
+{
+  name = "Catppuccin - Frappe";
+  author = "https://github.com/catppuccin/xresources";
+  colors = {
+    background = "#303446";
+    foreground = "#C6D0F5";
+    cursorColor = "#d4be98";
+    black = "#51576D";
+    red = "#E78284";
+    green = "#A6D189";
+    yellow = "#E5C890";
+    blue = "#8CAAEE";
+    magenta = "#F4B8E4";
+    cyan = "#81C8BE";
+    white = "#B5BFE2";
+    blackBright = "#626880";
+    redBright = "#E78284";
+    greenBright = "#A6D189";
+    yellowBright = "#E5C890";
+    blueBright = "#8CAAEE";
+    magentaBright = "#F4B8E4";
+    cyanBright = "#81C8BE";
+    whiteBright = "#A5ADCE";
+  };
+}
