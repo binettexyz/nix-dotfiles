675f79cbdfbee9e3b45732b903ecf4f66812e31d
Jonathan Binette <binettexyz@proton.me>
2023-03-13 14:22:24 -0400

    System: fix small icon issue

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -12,29 +12,33 @@
       noto-fonts-emoji
       material-design-icons
       material-icons
-      (nerdfonts.override {
-        fonts = [
-          "Iosevka"
-          "FiraCode"
-          "JetBrainsMono"
-          "Mononoki"
-          "FantasqueSansMono"
-        ];
-      })
+      fira-code
+        #FIXME: Nerd fonts make icons small and cant be bigger if increasing icon size. 
+#      (nerdfonts.override {
+#        fonts = [
+#          "Iosevka"
+#          "FiraCode"
+#          "JetBrainsMono"
+#          "Mononoki"
+#          "FantasqueSansMono"
+#        ];
+#      })
     ];
 
     fontconfig = {
       enable = true;
       includeUserConf = true;
       cache32Bit = true;
+        #TODO: Find better font instead of nerdfonts.
       defaultFonts = {
         emoji = [ "Noto Color Emoji" ];
-        serif = [ "JetBrainsMono Nerd Font Mono" ];
-        sansSerif = [ "JetBrainsMono Nerd Font Mono" ];
+#        serif = [ "JetBrainsMono Nerd Font Mono" ];
+#        sansSerif = [ "JetBrainsMono Nerd Font Mono" ];
         monospace = [
+          "FiraCode"
           #"FiraCode Nerd Font Mono"
           #"Iosevka Term"
-          "FantasqueSansMono Nerd Font Mono"
+          #"FantasqueSansMono Nerd Font Mono"
           #"mononoki Nerd Font Mono"
         ];
       };
