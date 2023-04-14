ec5516473e8ed3b271d7867a4dc55b66d7edae91
Jonathan Binette <binettexyz@proton.me>
2023-04-14 15:37:11 -0400

    Home+dwm: removed unnecessary stuff

--- home-manager/dwm/default.nix
+++ home-manager/dwm/default.nix
@@ -1,10 +1,7 @@
 { config, pkgs, inputs, lib, ... }:
 let
-  cfg = config.modules.type.laptop;
   dmenu = pkgs.callPackage (inputs.dmenu + "/default.nix") {};
   dwm = pkgs.callPackage (inputs.dwm + "/default.nix") {};
-#  slstatus-desktop = pkgs.callPackage (inputs.slstatus-desktop + "/default.nix") {};
-#  slstatus-laptop = pkgs.callPackage (inputs.slstatus-laptop + "/default.nix") {};
   st = pkgs.callPackage (inputs.st + "/default.nix") {};
 in
 {
@@ -20,6 +17,6 @@ in
     dmenu
     dwm
     st
-  ]; # ++ (if config.modules.device.type == "laptop" then [ slstatus-laptop ] else [ slstatus-desktop ]);
+  ];
 
 }
