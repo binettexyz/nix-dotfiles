49b01d1c990ea993b17f9f93c3768d063b0372fb
Jonathan Binette <binettexyz@proton.me>
2023-05-02 03:21:18 -0400

    SSH: add steamdeck public key

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -25,8 +25,9 @@ in
     };
   
       # Add SSH key
-    users.extraUsers.${username}.openssh.authorizedKeys.keys = [
+    users.users.${username}.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxsgVkgA8fBxOOsL8WmqGa1hAzYgl7YNz/OvLiDq5fO binette@desktop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWNbKHKSSjQAEGlWVhrPQ8vcolszTiwNKXB0FMEBtfw binette@decky"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH3Fo8FZrrTYGqIH84r67dkmSIxcOD3DhJrIJe6ndJ0z binette@t440p"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWeBglzVFzHXfNq/T+Hu31ukZ3iN6I5UXBpxIN7MRAP binette@x240"
     ];
