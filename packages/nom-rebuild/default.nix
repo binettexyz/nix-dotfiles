1ce6dc4b2f4be6b5d3df21ef1e01334e48d85f6f
Jonathan Binette <binettexyz@proton.me>
2023-05-16 12:42:35 -0400

    Packages: edit

--- packages/nom-rebuild/default.nix
+++ packages/nom-rebuild/default.nix
@@ -7,6 +7,6 @@ writeShellApplication {
   # Not including sudo/nixos-rebuild since they are included in NixOS base
   runtimeInputs = [ nix-output-monitor ];
   text = ''
-    nixos-rebuild "$@" |& nom
+    pushd /etc/nixos; doas nixos-rebuild "$@" --flake .#; popd |& nom
   '';
 }
