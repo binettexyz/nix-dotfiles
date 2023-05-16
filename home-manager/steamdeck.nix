e00e6f681af9e818fbc50d5c5da5aa90a5797312
Jonathan Binette <binettexyz@proton.me>
2023-05-16 12:15:14 -0400

    Home: add steamdeck's config

---
+++ home-manager/steamdeck.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./minimal.nix
+  ];
+
+  home.packages = with pkgs; [
+    wl-clipboard
+    discord
+    solaar
+    tidal-hifi
+  ];
+
+  programs.zsh.profileExtra =
+    let
+      inherit (config.home) homeDirectory;
+    in
+    ''
+      # Load nix environment
+      if [ -e "${homeDirectory}/.nix-profile/etc/profile.d/nix.sh" ]; then
+        . "${homeDirectory}/.nix-profile/etc/profile.d/nix.sh"
+      fi
+    '';
+}
