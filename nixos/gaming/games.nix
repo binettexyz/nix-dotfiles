7e8e07bd4bc6b43d8ccebf4db37bfe17c4fe66de
Jonathan Binette <binettexyz@proton.me>
2024-06-28 13:02:48 -0400

    System+Gaming: Add games ports to firewall

--- nixos/gaming/games.nix
+++ nixos/gaming/games.nix
@@ -1,4 +1,21 @@
-{ pkgs,... }: {
+{ pkgs,... }:
+
+let
+  ports = {
+    mindustry = 6567;
+    factorio = 6566;
+  };
+
+in {
+
+  networking.firewall.allowedTCPPorts = [
+    ports.mindustry
+    ports.factorio
+  ];
+  networking.firewall.allowedUDPPorts = [
+    ports.mindustry
+    ports.factorio
+  ];
 
   environment.systemPackages = with pkgs; [
     mindustry
