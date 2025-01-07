ca1813fb50c2e4ac2b49b05a0722d5274f79b464
Jonathan Binette <binettexyz@proton.me>
2025-01-07 12:30:50 -0500

    Host+Steamdeck: Changed hostname

--- hosts/steamdeck/config.nix
+++ hosts/steamdeck/config.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "steamdeck"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   time.timeZone = "America/New_York";
