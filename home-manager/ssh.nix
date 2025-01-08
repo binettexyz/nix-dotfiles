090b4d1bbd1253a6f4d4a112da773e6e88e02262
Jonathan Binette <binettexyz@proton.me>
2025-01-08 10:34:45 -0500

    Home-manager+SSH: Add desktop-server to host config.

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -6,12 +6,8 @@ with lib;
     programs.ssh = {
       enable = true;
       matchBlocks = {
-        "github.com" = {
-          hostname = "github.com";
-          identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
-        };
-        "nas" = {
-          hostname = "100.71.254.90";
+        "desktop-server" = {
+          hostname = "100.69.22.72";
           user = "${config.home.username}";
           port = 704;
         };
