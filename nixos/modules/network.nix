888e7edf5766555d62dc0a1f409bd15409ebcb53
Jonathan Binette <binettexyz@proton.me>
2025-02-10 22:14:08 -0500

    System: Revert back network config

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -1,56 +1,46 @@
 { config, lib, ... }: {
 
-  /* ---Enable/Disable network components--- */
   services.tailscale.enable = true;
+
   networking = {
     networkmanager.enable = lib.mkDefault false;
-    wireless.enable = lib.mkDefault true;
-    firewall.enable = lib.mkDefault true;
-  };
 
-  /* ---Tailscale--- */
-  networking = {
-    interfaces.tailscale0.useDHCP = true;
-    firewall.checkInterfaces = "loose";
-    firewall.trustedInterfaces = [ "tailscale0" ];
-  };
-
-  /* ---DNS--- */
-  networking = {
+    /* ---DNS--- */
     enableIPv6 = lib.mkDefault false;
     useDHCP = lib.mkDefault false;
-    nameservers = [ "100.69.22.72" ]; # AdGuardHome
-  };
+    nameservers = [ "100.69.22.72" ]; # adguardhome
 
-  /* ---Firewall--- */
-  networking.firewall = {
-    allowPing = false;
-    allowedTCPPorts = [
-      2049 # NFSv4
-      53 # dns
-      #80
-      #443
-    ];
-    allowedUDPPorts = [
-      config.services.tailscale.port
-      53 # dns
-      #80
-      #443
-    ];
-  };
+    /* ---Firewall--- */
+    firewall = {
+      enable = true;
+      allowPing = false;
+      allowedTCPPorts = [
+        2049 # NFSv4
+        53 # dns
+      ];
+      allowedUDPPorts = [
+        config.services.tailscale.port
+        53 # dns
+      ];
+        # tailscale
+      checkReversePath = "loose";
+      trustedInterfaces = [ "tailscale0" ];
+    };
 
-  /* ---Wifi--- */
-  networking.wireless = {
-    userControlled.enable = true;
-    networks."BELL248" = { # Home Wifi
-      priority = 0;
-      auth = ''
-        psk=9f41abf6a150bbf3eb7ca1bb6fb9a5fff55e788ce8ad3db634d4336746257e45
-        proto=RSN
-     	  key_mgmt=WPA-PSK
-        pairwise=CCMP
-        auth_alg=OPEN
-      '';
+    /* ---Wifi--- */
+    wireless = {
+      enable = lib.mkDefault true;
+      userControlled.enable = true;
+      networks."BELL248" = {
+        priority = 0;
+        auth = ''
+          psk=9f41abf6a150bbf3eb7ca1bb6fb9a5fff55e788ce8ad3db634d4336746257e45
+          proto=RSN
+       	  key_mgmt=WPA-PSK
+       	  pairwise=CCMP
+   	      auth_alg=OPEN
+        '';
+      };
     };
   };
 
