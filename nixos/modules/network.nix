5decaf44abef4a68ab3939f8f13ab352e4dda995
Jonathan Binette <binettexyz@proton.me>
2025-02-10 21:31:18 -0500

    System: Clean network config

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -1,73 +1,56 @@
 { config, lib, ... }: {
 
+  /* ---Enable/Disable network components--- */
   services.tailscale.enable = true;
+  networking = {
+    networkmanager.enable = lib.mkDefault false;
+    wireless.enable = lib.mkDefault true;
+    firewall.enable = lib.mkDefault true;
+  };
 
+  /* ---Tailscale--- */
+  networking = {
+    interfaces.tailscale0.useDHCP = true;
+    firewall.checkInterfaces = "loose";
+    firewall.trustedInterfaces = [ "tailscale0" ];
+  };
+
+  /* ---DNS--- */
   networking = {
     enableIPv6 = lib.mkDefault false;
     useDHCP = lib.mkDefault false;
-    networkmanager.enable = lib.mkDefault false;
-    nameservers = [
-      "100.69.22.72" # adguardhome dns using tailscale.
-#      "9.9.9.9"
-    ];
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        2049 # NFSv4
-        53 # dns
-#        80
-#        443
-      ];
-      allowedUDPPorts = [
-        config.services.tailscale.port
-        53 # dns
-#        80
-#        443
-      ];
-      allowPing = false;
-        # tailscale
-      checkReversePath = "loose";
-      trustedInterfaces = [ "tailscale0" ];
-    };
+    nameservers = [ "100.69.22.72" ]; # AdGuardHome
+  };
 
-    wireless = {
-      enable = lib.mkDefault true;
-      userControlled.enable = true;
-      networks = {
-          # Home
-        "BELL248" = {
-          priority = 0;
-          auth = ''
-           psk=9f41abf6a150bbf3eb7ca1bb6fb9a5fff55e788ce8ad3db634d4336746257e45
-           proto=RSN
-          	key_mgmt=WPA-PSK
-          	pairwise=CCMP
-          	auth_alg=OPEN
-          '';
-        };
-          # CHSLD Ste-Rose
-        "Loisirs" = {
-          priority = 2;
-          auth = ''
-            psk=ca62593afdd9ec7cac04d7730061da25616f3f883aca43a4245675947acd24fb
-            proto=RSN
-            key_mgmt=WPA-PSK
-            pairwise=CCMP
-            auth_alg=OPEN
-          '';
-        };
-          # Mobile Hotspot
-        "bin-hotspot" = {
-          priority = 3;
-          auth = ''
-            psk=82dd0b712d8928c6cc5f019c16cd0feab6271cf4e3505781914a5bb6cb41b63e
-            proto=RSN
-            key_mgmt=WPA-PSK
-            pairwise=CCMP
-            auth_alg=OPEN
-          '';
-        };
-      };
+  /* ---Firewall--- */
+  networking.firewall = {
+    allowPing = false;
+    allowedTCPPorts = [
+      2049 # NFSv4
+      53 # dns
+      #80
+      #443
+    ];
+    allowedUDPPorts = [
+      config.services.tailscale.port
+      53 # dns
+      #80
+      #443
+    ];
+  };
+
+  /* ---Wifi--- */
+  networking.wireless = {
+    userControlled.enable = true;
+    networks."BELL248" = { # Home Wifi
+      priority = 0;
+      auth = ''
+        psk=9f41abf6a150bbf3eb7ca1bb6fb9a5fff55e788ce8ad3db634d4336746257e45
+        proto=RSN
+     	  key_mgmt=WPA-PSK
+        pairwise=CCMP
+        auth_alg=OPEN
+      '';
     };
   };
 
