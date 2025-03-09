c7976708e2e475c73454923891fb969c3a13a770
Jonathan Binette <binettexyz@proton.me>
2025-03-08 20:26:40 -0500

    System: Removed plymouth from bootloader's config

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -24,39 +24,35 @@ in {
   /* ---Configuration--- */
   config = mkMerge [
     (mkIf (cfg == "grub") {
-      boot = {
-        plymouth.enable = false;
-        loader = {
-          timeout = 1;
-          efi = {
-            canTouchEfiVariables = if config.modules.bootloader.asRemovable then false else true;
-            efiSysMountPoint = "/boot";
-          };
-        # Grub bootloader
-          grub = {
-            enable = true;
-            default = 0;
-            device = "nodev";
-            efiInstallAsRemovable = config.modules.bootloader.asRemovable;
-            efiSupport = true;
-            gfxmodeEfi = "1280x800";
-            useOSProber = config.modules.bootloader.useOSProber;
-            backgroundColor = lib.mkDefault "#000000";
-            splashImage = null;
-            splashMode = "normal";
-            extraConfig = ''
-              set menu_color_normal=white/black
-              set menu_color_highlight=black/white
-            '';
-            extraEntries = ''
-              menuentry "Poweroff" {
-  	            halt
-             }
-  	          menuentry "Reboot" {
-               reboot
-              }
-            '';
-          };
+      boot.loader = {
+        timeout = 1;
+        efi = {
+          canTouchEfiVariables = if config.modules.bootloader.asRemovable then false else true;
+          efiSysMountPoint = "/boot";
+        };
+        grub = {
+          enable = true;
+          default = 0;
+          device = "nodev";
+          efiInstallAsRemovable = config.modules.bootloader.asRemovable;
+          efiSupport = true;
+          gfxmodeEfi = "1280x800";
+          useOSProber = config.modules.bootloader.useOSProber;
+          backgroundColor = lib.mkDefault "#000000";
+          splashImage = null;
+          splashMode = "normal";
+          extraConfig = ''
+            set menu_color_normal=white/black
+            set menu_color_highlight=black/white
+          '';
+          extraEntries = ''
+            menuentry "Poweroff" {
+  	          halt
+           }
+  	        menuentry "Reboot" {
+             reboot
+            }
+          '';
         };
       };
     })
