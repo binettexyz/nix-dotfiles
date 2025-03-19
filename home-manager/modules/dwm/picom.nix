7bf05083c0a26b91af63d48743ca0ffb0b0ab16e
Jonathan Binette <binettexyz@proton.me>
2025-03-18 22:46:50 -0400

    Home-Manager+Picom: Removed Useless package

--- home-manager/modules/dwm/picom.nix
+++ home-manager/modules/dwm/picom.nix
@@ -9,14 +9,6 @@ in {
       # Enabled client-side shadows on windows. 
     enable = true;
     backend = "glx";
-    package = pkgs.picom.overrideAttrs(o: {
-      src = pkgs.fetchFromGitHub {
-        repo = "picom";
-        owner = "ibhagwan";
-        rev = "c4107bb6cc17773fdc6c48bb2e475ef957513c7a";
-        sha256 = "sha256-1hVFBGo4Ieke2T9PqMur1w4D0bz/L3FAvfujY9Zergw=";
-      };
-    });
 
     /* Opacity */
     activeOpacity = 0.7; # (0.0 - 1.0 )
