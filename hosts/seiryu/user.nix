5efc2c8a4365a76466f101aa56371f8c9397a985
Jonathan Binette <binettexyz@proton.me>
2025-03-28 23:48:37 -0400

    Host+Home-Manager: Removed unused option

--- hosts/seiryu/user.nix
+++ hosts/seiryu/user.nix
@@ -12,48 +12,4 @@ in {
 
   colorScheme = import ../../modules/colorSchemes/gruvbox-material.nix;
 
-  xresources.properties =
-    let
-      fontSize = 12;
-    in {
-    /* --- XFT --- */
-    "Xft.dpi" = 96;
-    /* --- Xterm --- */
-      # Font
-    "xterm*faceName" = "FantasqueSansMono Nerd Font Mono";
-    "xterm*faceSize" = fontSize;
-
-    /* --- Xresources --- */
-      # Font
-    "*.font" = "monospace:size=${toString fontSize}";
-
-    "*.background" = "#${config.colorScheme.colors.background}";
-    "*.foreground" = "#${config.colorScheme.colors.foreground}";
-    "*.cursorColor" = "#${config.colorScheme.colors.cursorColor}";
-      # Black + DarkGrey
-    "*.color0"  = "#${config.colorScheme.colors.black}";
-    "*.color8" = "#${config.colorScheme.colors.blackBright}";
-      # DarkRed + Red
-    "*.color1" = "#${config.colorScheme.colors.red}";
-    "*.color9" = "#${config.colorScheme.colors.redBright}";
-      # DarkGreen + Green
-    "*.color2" = "#${config.colorScheme.colors.green}";
-    "*.color10" = "#${config.colorScheme.colors.greenBright}";
-      # DarkYellow + Yellow
-    "*.color3" = "#${config.colorScheme.colors.yellow}";
-    "*.color11" = "#${config.colorScheme.colors.yellowBright}";
-      # DarkBlue + Blue
-    "*.color4" = "#${config.colorScheme.colors.blue}";
-    "*.color12" = "#${config.colorScheme.colors.blueBright}";
-      # DarkMagenta + Magenta
-    "*.color5" = "#${config.colorScheme.colors.magenta}";
-    "*.color13" = "#${config.colorScheme.colors.magentaBright}";
-      # DarkCyan + Cyan
-    "*.color6" = "#${config.colorScheme.colors.cyan}";
-    "*.color14" = "#${config.colorScheme.colors.cyanBright}";
-      # LightGrey + White
-    "*.color7" = "#${config.colorScheme.colors.white}";
-    "*.color15" = "#${config.colorScheme.colors.whiteBright}";
-  };
-
 }
