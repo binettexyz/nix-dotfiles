ea3e564bfd00ce0558e634bf02d1ccbf87e1ff3b
Jonathan Binette <binettexyz@proton.me>
2025-04-01 10:29:43 -0400

    Home-Manager+Qtile: Add autostart script

---
+++ home-manager/modules/dotfiles/qtile/etc/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$(hostname)" = "seiryu" ]; then
+    (
+      sleep 5 &&
+      wlr-randr --output HDMI-A-1 --pos 0,0    --mode 1920x1080@179.981995 \
+                --output HDMI-A-2 --pos 1920,0 --mode 3840x2160@120 --off
+    )
+fi
