15576c400e26942388eb948747d00882c862a4f1
Jonathan Binette <binettexyz@proton.me>
2025-04-02 08:44:33 -0400

    Reduced nesting

--- nixos/gaming-handheld.nix
+++ nixos/gaming-handheld.nix
@@ -3,17 +3,13 @@
   imports = [ ./modules ];
 
   /* ---Custom Modules--- */
-  device = {
-    type = "gaming-handheld";
-    hasBattery = true;
-  };
-  modules.gaming = {
-    enable = true;
-    steam.enable = true;
-  };
-  modules.system = {
-    audio.enable = true;
-    home.enable = true;
+  device.type = "gaming-handheld";
+  device.hasBattery = true;
+  modules = {
+    gaming.enable = true;
+    gaming.steam.enable = true;
+    system.audio.enable = true;
+    system.home.enable = true;
   };
 
   /* ---System Configuration--- */
