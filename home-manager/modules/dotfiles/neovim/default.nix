c7edcca406ff13d4bc0e3bcafeec738b3bcb5abc
Jonathan Binette <binettexyz@proton.me>
2025-04-06 00:34:09 -0400

    Home-Manager: Clean config

--- home-manager/modules/dotfiles/neovim/default.nix
+++ home-manager/modules/dotfiles/neovim/default.nix
@@ -1,84 +1,91 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 with lib;
 
 {
 
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      withPython3 = true;
-      withRuby = false;
-      withNodeJs = false;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+    withRuby = false;
+    withNodeJs = false;
 
-      plugins = with pkgs.vimPlugins; [
-            # Language servers
-          vim-nix
-            # Quality of life
-          colorizer
-          vim-illuminate
-          auto-pairs
-          quick-scope
-          telescope-nvim
-        ];
+    plugins = with pkgs.vimPlugins; [
+      # Language servers
+      vim-nix
+      # Quality of life
+      colorizer
+      vim-illuminate
+      auto-pairs
+      quick-scope
+      telescope-nvim
+    ];
 
-      extraConfig = builtins.replaceStrings
-        ["<COLOURSCHEME>"] ["gruvbox-material"]
-        (builtins.readFile ./config.vim);
+    extraConfig = builtins.replaceStrings [ "<COLOURSCHEME>" ] [ "gruvbox-material" ] (
+      builtins.readFile ./src/config.vim
+    );
 
-        # TODO: convert theses to lua.
-#      extraConfig = lib.strings.concatStringsSep "\n" [
-#        ''
-#            " Disables automatic commenting on newline:
-#          autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-#        ''
-#          # Custom Fontions
-#        ''    
-#            " Save file as sudo on files that require root permission
-#          command! W execute 'w !doas tee % > /dev/null' <bar> edit!
-#    
-#            " Check file in shellcheck:
-#    	    map <leader>s :!clear && shellcheck -x %<CR>
-#    
-#            " Compile document, be it groff/LaTeX/markdown/etc.
-#    	    map <leader>c :w! \| !compiler "<c-r>%"<CR>
-#    
-#          autocmd BufRead,BufNewFile /tmp/calcurse*,~/.calcurse/notes/* set filetype=markdown
-#          autocmd BufRead,BufNewFile *.ms,*.me,*.mom,*.man set filetype=groff
-#          autocmd BufRead,BufNewFile *.tex set filetype=tex
-#          autocmd BufRead,BufNewFile *.nix set filetype=nix
+    # TODO: convert theses to lua.
+    #      extraConfig = lib.strings.concatStringsSep "\n" [
+    #        ''
+    #            " Disables automatic commenting on newline:
+    #          autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+    #        ''
+    #          # Custom Fontions
+    #        ''
+    #            " Save file as sudo on files that require root permission
+    #          command! W execute 'w !doas tee % > /dev/null' <bar> edit!
+    #
+    #            " Check file in shellcheck:
+    #    	    map <leader>s :!clear && shellcheck -x %<CR>
+    #
+    #            " Compile document, be it groff/LaTeX/markdown/etc.
+    #    	    map <leader>c :w! \| !compiler "<c-r>%"<CR>
+    #
+    #          autocmd BufRead,BufNewFile /tmp/calcurse*,~/.calcurse/notes/* set filetype=markdown
+    #          autocmd BufRead,BufNewFile *.ms,*.me,*.mom,*.man set filetype=groff
+    #          autocmd BufRead,BufNewFile *.tex set filetype=tex
+    #          autocmd BufRead,BufNewFile *.nix set filetype=nix
 
-#" Runs a script that cleans out tex build files whenever I close out of a .tex file.
-#	autocmd VimLeave *.tex !texclear %
-    };
+    #" Runs a script that cleans out tex build files whenever I close out of a .tex file.
+    #	autocmd VimLeave *.tex !texclear %
+  };
 
-      imports = [
+  imports = [
 
-        ./plugins/catppuccin-nvim
+    ./src/plugins/catppuccin-nvim
 
-        # TODO: replace coc by nvim native lsp
-        ./plugins/coc-nvim
+    # TODO: replace coc by nvim native lsp
+    ./src/plugins/coc-nvim
 
-        ./plugins/gruvbox-material
-        
-        ./plugins/lualine
+    ./src/plugins/gruvbox-material
 
-        ./plugins/nvim-tree-lua
+    ./src/plugins/lualine
 
-        ./plugins/nvim-treesitter
+    ./src/plugins/nvim-tree-lua
 
-        ./plugins/orgmode
+    ./src/plugins/nvim-treesitter
 
-        ./plugins/vim-latex-live-preview
+    ./src/plugins/orgmode
 
-        ./plugins/vimsence
+    ./src/plugins/vim-latex-live-preview
 
-        #FIXME: ./plugins/vimtex
+    ./src/plugins/vimsence
 
-      ];
+    #FIXME: ./src/plugins/vimtex
 
+  ];
 
-  
-    home.packages = with pkgs; [ /*rnix-lsp*/ ];
+  home.packages =
+    with pkgs;
+    [
+      # rnix-lsp
+    ];
 }
--- home-manager/modules/dotfiles/qtile/default.nix
+++ home-manager/modules/dotfiles/qtile/default.nix
@@ -1,8 +1,8 @@
-{ lib, super, ... }: {
+{ lib, super, ... }:
+{
 
   config = lib.mkIf super.services.xserver.windowManager.qtile.enable {
-    home.file.".config/qtile/config.py".source = ./etc/config.py;
-    
+    home.file.".config/qtile/config.py".source = ./src/config.py;
 
   };
 }
