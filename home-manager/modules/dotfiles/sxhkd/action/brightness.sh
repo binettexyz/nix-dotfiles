a135b7dc1f8b7d946aa652d25964f5f9240b90ac
Jonathan Binette <binettexyz@proton.me>
2025-04-06 00:17:59 -0400

    Home-Manager: Removed sxhkd

--- home-manager/modules/dotfiles/sxhkd/action/brightness.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VALUE=15
-
-case "$1" in
-	up)   doas light -A $VALUE;;
-	down) doas light -U $VALUE;;
-esac
--- home-manager/modules/dotfiles/sxhkd/action/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VALUE=5
-
-case "$1" in
-	mute) pamixer -t;;
-	up)   pamixer --allow-boost -i $VALUE;;
-	down) pamixer --allow-boost -d $VALUE;;
-esac
