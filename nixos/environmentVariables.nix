b953e46723f0613bd304305633a8601f3670b4ee
Jonathan Binette <binettexyz@proton.me>
2025-04-08 22:31:12 -0400

    System: Changed  of emacs

--- nixos/environmentVariables.nix
+++ nixos/environmentVariables.nix
@@ -1,45 +1,49 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 
 {
 
-    environment.localBinInPath = true;
-    environment.variables = {
-      # Default Programs:
-      EDITOR = "hx";
-      VISUAL = "hx";
-      TERMINAL = if config.device == "laptop" then "st" else "konsole";
-      BROWSER = if config.device == "laptop" then "qutebrowser" else "librefox";
-      READER = if config.device == "laptop" then "zatura" else "";
-      SUDO = "doas";
-      MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-      SHELL = "zsh";
+  environment.localBinInPath = true;
+  environment.variables = {
+    # Default Programs:
+    EDITOR = "emacsclient -t -a ''";
+    VISUAL = "emacsclient -c -a emacs";
+    TERMINAL = "foot";
+    BROWSER = if config.device == "laptop" then "qutebrowser" else "librefox";
+    READER = "zathura";
+    SUDO = "doas";
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    SHELL = "zsh";
 
-      # Themes
-      BAT_THEME = "ansi";
-      LS_COLORS = "lf";
-  
-      # Default Home Directories:
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_CACHE_HOME = "$HOME/.cache";
-      INPUTRC = "$HOME/.config/shell/inputrc";
-      #KDEHOME = "$HOME/.config/kde";
-  
-      # ~/ Clean-up:
-      XINITRC = "$HOME/.config/x11/xinitrc";
-      XAUTHORITY = "$XDG_RUNTIME_DIR/Xauthority"; # This line will break some DMs.
-      GTK2_RC_FILES = "$HOME/.config/gtk-2.0/gtkrc-2.0";
-      LESSHISTFILE = "-";
-      ZDOTDIR = "$HOME/.config/zsh";
-      GNUPGHOME = "$HOME/.local/share/gnupg";
-      PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
-      TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
-      HISTFILE = "$HOME/.local/share/history";
-  
-      # Other Program Settings:
-      MOZ_USE_XINPUT2 = "1";
-      _JAVA_AWT_WM_NONREPARENTING = 1;
-    };
+    # Themes
+    BAT_THEME = "ansi";
+    LS_COLORS = "lf";
+
+    # Default Home Directories:
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    INPUTRC = "$HOME/.config/shell/inputrc";
+
+    # ~/ Clean-up:
+    XINITRC = "$HOME/.config/x11/xinitrc";
+    XAUTHORITY = "$XDG_RUNTIME_DIR/Xauthority"; # This line will break some DMs.
+    GTK2_RC_FILES = "$HOME/.config/gtk-2.0/gtkrc-2.0";
+    LESSHISTFILE = "-";
+    ZDOTDIR = "$HOME/.config/zsh";
+    GNUPGHOME = "$HOME/.local/share/gnupg";
+    PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+    TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
+    HISTFILE = "$HOME/.local/share/history";
+
+    # Other Program Settings:
+    MOZ_USE_XINPUT2 = "1";
+    _JAVA_AWT_WM_NONREPARENTING = 1;
+  };
 
 }
