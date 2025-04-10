04f9a88a2bcbe965a3927475f6e87df621d70de6
Jonathan Binette <binettexyz@proton.me>
2025-04-09 22:56:19 -0400

    Home-Manager+Qtile: Fix blackscreen with autostart on boot

--- home-manager/dotfiles/qtile/default.nix
+++ home-manager/dotfiles/qtile/default.nix
@@ -3,6 +3,9 @@
 
   config = lib.mkIf super.services.xserver.windowManager.qtile.enable {
     home.file.".config/qtile/config.py".source = ./src/config.py;
-
+    home.file.".config/qtile/autostart.sh" = {
+      source = ./src/autostart.sh;
+      executable = true;
+    };
   };
 }
--- home-manager/dotfiles/qtile/src/autostart.sh
+++ home-manager/dotfiles/qtile/src/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 if [ "$(hostname)" = "seiryu" ]; then
-    (
-      sleep 5 &&
+    {
+      sleep 5 && \
       wlr-randr --output HDMI-A-1 --pos 0,0    --mode 1920x1080@179.981995 \
                 --output HDMI-A-2 --pos 1920,0 --mode 3840x2160@120 --off
-      sleep 5 &&
-      /etc/profiles/per-user/binette/bin/emacs --daemon &
-    )
+      emacs --daemon &
+    } &
 fi
