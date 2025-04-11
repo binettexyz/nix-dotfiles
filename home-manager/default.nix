9fdf9caab411a0aa4f3b0254bab7d7e4e172e8a1
Jonathan Binette <binettexyz@proton.me>
2025-04-11 11:29:53 -0400

    Cleanup config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -4,21 +4,22 @@
   imports = [
     ./meta
     ./packages.nix
-    ./dotfiles/chromium.nix
-    ./dotfiles/emacs
-    ./dotfiles/foot.nix
-    ./dotfiles/git.nix
-    ./dotfiles/helix.nix
-    ./dotfiles/lf
-    ./dotfiles/librewolf.nix
-    ./dotfiles/mpv
-    ./dotfiles/neovim
-    ./dotfiles/newsboat.nix
-    ./dotfiles/qtile
-    ./dotfiles/qutebrowser
-    ./dotfiles/ssh.nix
-    ./dotfiles/tmux.nix
-    ./dotfiles/zsh.nix
+    ./dotfiles/browsers/chromium.nix
+    ./dotfiles/browsers/librewolf.nix
+    ./dotfiles/browsers/qutebrowser
+    ./dotfiles/desktopEnvironment/plasma-manager.nix
+    ./dotfiles/desktopEnvironment/qtile
+    ./dotfiles/media/mpv
+    ./dotfiles/media/newsboat.nix
+    ./dotfiles/shells/zsh.nix
+    ./dotfiles/terminals/foot.nix
+    ./dotfiles/textEditors/emacs
+    ./dotfiles/textEditors/helix.nix
+    ./dotfiles/textEditors/neovim
+    ./dotfiles/tools/git.nix
+    ./dotfiles/tools/lf
+    ./dotfiles/tools/ssh.nix
+    ./dotfiles/tools/tmux.nix
   ];
 
 }
