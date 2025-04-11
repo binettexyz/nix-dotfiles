f0dbfb07f6e772aa70311f22936861f438c66bc4
Jonathan Binette <binettexyz@proton.me>
2025-04-11 11:33:21 -0400

    System+Gaming: Add Gamescope

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -115,6 +115,11 @@ in
         protontricks.enable = true;
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
+        gamescopeSession.enable = true;
+      };
+      programs.gamescope = {
+        enable = true;
+        capSysNice = true;
       };
 
       hardware.steam-hardware.enable = true;
