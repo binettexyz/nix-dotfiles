15d203e911875c7cdab370848eafa1f64db9d50c
Jonathan Binette <binettexyz@proton.me>
2025-04-11 11:32:23 -0400

    Home-manager+Plasma-Manager: Add plasma-manager and it's config

---
+++ home-manager/dotfiles/desktopEnvironment/plasma-manager.nix
@@ -0,0 +1,103 @@
+{ pkgs, ... }: {
+  programs.plasma = {
+    enable = true;
+    overrideConfig = true;
+    workspace = {
+      clickItemTo = "open";
+      colorScheme = "BreezeDark";
+      cursor = {
+        theme = "Breeze_cursors";
+        size = 24;
+      };
+      iconTheme = "breeze-dark";
+      lookAndFeel = "org.kde.breezedark.desktop";
+      theme = "breeze-dark";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Mountain/contents/images_dark/5120x2880.png";
+    };
+
+    kscreenlocker.autoLock = false;
+
+    panels = [
+      {
+        location = "bottom";
+        alignment = "center";
+        hiding = "none";
+        floating = true;
+        widgets = [
+          {
+            kickoff = {
+              sortAlphabetically = true;
+              icon = "nix-snowflake-white";
+            };
+          }
+          {
+            iconTasks = {
+              launchers = [
+                "applications:librewolf.desktop"
+                "applications:org.kde.dolphin.desktop"
+                "applications:org.kde.konsole.desktop"
+                "applications:discord.desktop"
+                "applications:steam.desktop"
+                
+              ];
+              behavior = {
+                showTasks = {
+                  onlyInCurrentScreen = true;
+                  onlyInCurrentDesktop = true;
+                  onlyInCurrentActivity = true;
+                };
+              };
+            };
+          }
+          {
+            systemTray.items = {
+              shown = [
+                "org.kde.plasma.battery"
+              ];
+              hidden = [
+                "org.kde.plasma.addons.katesessions"
+              ];
+              configs = {
+                battery.showPercentage = true;
+              };
+            };
+          }
+          {
+            digitalClock.time = {
+              showSeconds = "always";
+              format = "24h";
+            };
+            digitalClock.calendar = {
+              firstDayOfWeek = "sunday";
+              showWeekNumbers = true;
+              plugins = [ "holidaysevents" ];
+            };
+          }
+          "org.kde.plasma.showdesktop"
+        ];
+      }
+    ];
+
+    powerdevil.AC = {
+      autoSuspend.action = "nothing";
+      dimDisplay.enable = false;
+      powerButtonAction = "sleep";
+      powerProfile = "performance";
+      turnOffDisplay.idleTimeout = "never";
+    };
+
+    configFile = {
+      kcminputrc."Libinput/1133/50503/Logitech USB Receiver" = {
+        PointerAccelerationProfile = 1;
+        PointerAcceleration = -0.200;    
+      };
+      kwinrc.Plugins.shakecursorEnabled = false;
+    };
+
+    dataFile = {
+      #"dolphin/view_properties/global/.directory"."Dolphin"."SortRole" = "modificationtime";
+      "dolphin/view_properties/global/.directory"."Dolphin"."ViewMode" = 1;
+      "dolphin/view_properties/global/.directory"."Settings"."HiddenFilesShown" = true;
+    };
+  }; 
+}
--- hosts/seiryu/user.nix
+++ hosts/seiryu/user.nix
@@ -8,6 +8,7 @@ in
     ../../home-manager
     (flake.inputs.impermanence + "/home-manager.nix")
     flake.inputs.nix-colors.homeManagerModule
+    flake.inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
 
   colorScheme = import ../../modules/colorSchemes/gruvbox-material.nix;
