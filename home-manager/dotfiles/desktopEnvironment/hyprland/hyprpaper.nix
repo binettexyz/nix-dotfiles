29674218090c25a3c1528cb9ee908ef24134a170
Jonathan Binette <binettexyz@proton.me>
2025-04-15 14:12:56 -0400

    Home-Manager+hyprpaper: Add hyprpaper (hyprland wallpaper utility)

---
+++ home-manager/dotfiles/desktopEnvironment/hyprland/hyprpaper.nix
@@ -0,0 +1,11 @@
+{
+
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [ "/home/binette/Pictures/wallpapers/gruvbox/001.png" ];
+      wallpaper = [ "HDMI-A-1,/home/binette/Pictures/wallpapers/gruvbox/001.png" ];
+    };
+  };
+
+}
