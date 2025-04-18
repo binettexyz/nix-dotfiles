05a280b4f73249ad7faf8fe08e657b6cd8f596c6
Jonathan Binette <binettexyz@proton.me>
2025-04-17 23:19:38 -0400

    Home-Manager+foot: Change opacity to fit hyprland

--- home-manager/dotfiles/terminals/foot.nix
+++ home-manager/dotfiles/terminals/foot.nix
@@ -1,4 +1,8 @@
-{ pkgs, config, lib, super, ... }: {
+{
+  config,
+  ...
+}:
+{
 
   programs.foot = {
     enable = true;
@@ -20,31 +24,34 @@
       key-bindings = {
         #clipboard-copy = "Control+c XF86Copy";
         #clipboard-paste = "Control+v XF86Paste";
+        font-increase = "Control+plus Control+equal Control+KP_Add";
+        font-decrease = "Control+minus Control+KP_Subtract";
+        font-reset = "Control+0 Control+KP_0";
       };
       colors = {
-        alpha = "0.97";
+        alpha = "0.9";
         background = "${config.colorScheme.palette.background}";
         foreground = "${config.colorScheme.palette.foreground}";
         flash = "7f7f00";
         flash-alpha = "0.5";
         # Normal/regular colors (color palette 0-7)
-        regular0 = "${config.colorScheme.palette.black}";  # black
-        regular1 = "${config.colorScheme.palette.red}";  # red
-        regular2 = "${config.colorScheme.palette.green}";  # green
-        regular3 = "${config.colorScheme.palette.yellow}";  # yellow
-        regular4 = "${config.colorScheme.palette.blue}";  # blue
-        regular5 = "${config.colorScheme.palette.magenta}";  # magenta
-        regular6 = "${config.colorScheme.palette.cyan}";  # cyan
-        regular7 = "${config.colorScheme.palette.white}";  # white
+        regular0 = "${config.colorScheme.palette.black}"; # black
+        regular1 = "${config.colorScheme.palette.red}"; # red
+        regular2 = "${config.colorScheme.palette.green}"; # green
+        regular3 = "${config.colorScheme.palette.yellow}"; # yellow
+        regular4 = "${config.colorScheme.palette.blue}"; # blue
+        regular5 = "${config.colorScheme.palette.magenta}"; # magenta
+        regular6 = "${config.colorScheme.palette.cyan}"; # cyan
+        regular7 = "${config.colorScheme.palette.white}"; # white
         # Bright colors (color palette 8-15)
-        bright0 = "${config.colorScheme.palette.blackBright}";   # bright black
-        bright1 = "${config.colorScheme.palette.redBright}";   # bright red
-        bright2 = "${config.colorScheme.palette.greenBright}";   # bright green
-        bright3 = "${config.colorScheme.palette.yellowBright}";   # bright yellow
-        bright4 = "${config.colorScheme.palette.blueBright}";   # bright blue
-        bright5 = "${config.colorScheme.palette.magentaBright}";   # bright magenta
-        bright6 = "${config.colorScheme.palette.cyanBright}";   # bright cyan
-        bright7 = "${config.colorScheme.palette.whiteBright}";   # bright white
+        bright0 = "${config.colorScheme.palette.blackBright}"; # bright black
+        bright1 = "${config.colorScheme.palette.redBright}"; # bright red
+        bright2 = "${config.colorScheme.palette.greenBright}"; # bright green
+        bright3 = "${config.colorScheme.palette.yellowBright}"; # bright yellow
+        bright4 = "${config.colorScheme.palette.blueBright}"; # bright blue
+        bright5 = "${config.colorScheme.palette.magentaBright}"; # bright magenta
+        bright6 = "${config.colorScheme.palette.cyanBright}"; # bright cyan
+        bright7 = "${config.colorScheme.palette.whiteBright}"; # bright white
       };
     };
   };
