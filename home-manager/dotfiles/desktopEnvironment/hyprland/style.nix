7ee6e01ec8207e395cb649da786c7303a2c0e779
Jonathan Binette <binettexyz@proton.me>
2025-04-17 23:15:44 -0400

    Home-Manager+waybar: Add waybar modules with its style.css file

---
+++ home-manager/dotfiles/desktopEnvironment/hyprland/style.nix
@@ -0,0 +1,152 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.colorScheme.palette;
+  hexToRgba = import ../../../../lib/hex2rgba.nix { inherit lib; };
+in
+{
+
+  programs.waybar.style = ''
+
+    * {
+      font-family: monospace;
+      font-size: 14px;
+      font-weight: 600;
+      min-height: 25px;
+      border-radius: 4px;
+    }
+
+    window#waybar {
+      background: ${
+        hexToRgba {
+          hexColor = cfg.background;
+          opacity = 0.96;
+        }
+      };
+    }
+
+    .modules-left {
+      border: 0px;
+      margin: 4px 0px 4px 4px;
+    }
+
+    .modules-right {
+      border: 0px;
+      margin: 4px 0px 4px 4px;
+    }
+
+    .modules-center {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 4px;
+      margin: 4px 0px 4px 0px;
+      padding: 0px 0px 0px 8px;
+    }
+
+    #custom-sep {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 0px;
+      border-right: 0px;
+      border-left: 0px;
+      margin-left: 0px;
+      padding: 0px 0px 0px 0px;
+    }
+
+    #clock {
+      background: #282828;
+      border-radius: 0px 4px 4px 0px;
+    }
+
+    #custom-nixos {
+      font-size: 20px;
+      color: #7daea3;
+      background: #282828;
+      border-radius: 4px;
+      padding: 0px 0px 0px 5px;
+      margin: 0px 4px 0px 0px;
+    }
+
+    #cpu {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 4px 0px 0px 4px;
+      border-right: 0px;
+      padding: 0px 4px 0px 4px;
+      min-width: 18px;
+    }
+
+    #memory {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 0px 0px 0px 0px;
+      border-left: 0px;
+      border-right: 0px;
+      padding: 0px 4px 0px 4px;
+    }
+
+    #temperature {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 0px 4px 4px 0px;
+      border-left: 0px;
+      padding: 0px 4px 0px 4px;
+    }
+
+    #workspaces {
+      background: #282828;
+      border-radius: 4px;
+      margin-right: 4px;
+    }
+
+    #workspaces button {
+      color: #d4be98;
+      padding: 0;
+      margin-right: 0px;
+    }
+
+    #workspaces button.active {
+      color: #7daea3;
+    }
+
+    #workspaces button.empty {
+      color: #928374;
+    }
+
+    #workspaces button.empty.active {
+      color: #7daea3;
+    }
+
+    #network {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 4px 0px 0px 4px;
+      border-right: 0px;
+      padding: 0px 4px 0px 4px;
+      min-width: 18px;
+    }
+
+    #pulseaudio {
+      color: #d4be98;
+      background: #282828;
+      border-radius: 0px 0px 0px 0px;
+      border-left: 0px;
+      border-right: 0px;
+      padding: 0px 4px 0px 4px;
+    }
+
+    #custom-power {
+      background: #282828;
+      color: #ea6962;
+      border-radius: 0px 4px 4px 0px;
+      border-left: 0px;
+      padding: 0px 4px 0px 4px;
+      margin: 0px 4px 0px 0px;
+    }
+
+  '';
+
+}
---
+++ home-manager/dotfiles/desktopEnvironment/hyprland/waybar.nix
@@ -0,0 +1,175 @@
+{ deviceType, ... }:
+{
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        margin = "8px, 8px, 0px, 8px";
+        reload_style_on_change = true;
+        output = [ "HDMI-A-1" ];
+        #include = [ ~/.config/waybar/modules.json ];
+        modules-left = [
+          "custom/nixos"
+          "cpu"
+          "custom/sep"
+          "memory"
+          "custom/sep"
+          "temperature"
+        ];
+        modules-center = [ "clock" ];
+        modules-right =
+          [
+            "hyprland/workspaces"
+          ]
+          ++ (
+            if deviceType == "laptop" then
+              [
+                "custom/sep"
+                "battery"
+                "battery#bat2"
+                "custom/sep"
+              ]
+            else
+              [ ]
+          )
+          ++ [
+            "network"
+            "custom/sep"
+            "pulseaudio"
+            "pulseaudio#mic"
+            "custom/sep"
+            "custom/power"
+          ];
+
+        # ---Modules---
+        "custom/nixos" = {
+          format = " ";
+          tooltip = false;
+        };
+        "custom/sep" = {
+          format = "|";
+          tooltip = false;
+        };
+        "custom/power" = {
+          format = " ";
+          tooltip = false;
+        };
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          warp-on-scroll = false;
+          format = "{name}";
+          format-icons = {
+            urgent = "";
+            active = "";
+            default = "";
+          };
+          persistent-workspaces = {
+            "*" = 3;
+          };
+          tooltip = false;
+        };
+        pulseaudio = {
+          format = "{icon} {volume}% -";
+          format-bluetooth = "{icon} {volume}%  {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " Muted -";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            default = [
+              ""
+              ""
+              ">"
+            ];
+          };
+          on-scroll-up = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0";
+          on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0";
+          on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          tooltip = false;
+        };
+        "pulseaudio#mic" = {
+          format = "{format_source}";
+          format-source = " {volume}%";
+          format-source-muted = " Muted";
+          on-scroll-up = "wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 5%+ -l 1.0";
+          on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 5%- -l 1.0";
+          on-click = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          tooltip = false;
+        };
+        network = {
+          format-wifi = "{essid}  ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ";
+          on-click = "sh ~/scripts/rofi-wifi-menu/rofi-wifi-menu.sh";
+          tooltip = false;
+        };
+        cpu = {
+          format = " {usage}%";
+          tooltip = false;
+        };
+        memory = {
+          format = " {}%";
+          tooltip = false;
+        };
+        temperature = {
+          interval = 10;
+          hwmon-path = "/sys/devices/platform/coretemp.0/hwmon/hwmon4/temp1_input";
+          critical-threshold = 100;
+          format-critical = " {temperatureC}";
+          format = " {temperatureC}°C";
+          tooltip = false;
+        };
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          tooltip = false;
+          format = "{icon}  {capacity}%";
+          format-full = "{icon}  {capacity}%";
+          format-charging = "  {capacity}%";
+          format-plugged = "  {capacity}%";
+          format-alt = "{time}  {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+        "battery#bat2" = {
+          bat = "BAT2";
+          tooltip = false;
+        };
+        clock = {
+          format = "{:%A - %B %d, %Y - %R} ";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          format-alt = "{:%Y-%m-%d}";
+          tooltip = false;
+        };
+        bluetooth = {
+          format = " {status}";
+          format-connected = " {device_alias}";
+          format-connected-battery = " {device_alias} {device_battery_percentage}%";
+          format-device-preference = [
+            "device1"
+            "device2"
+          ];
+          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
+          tooltip = false;
+        };
+      };
+      #style = { };
+    };
+  };
+}
