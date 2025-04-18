dc5f3ab3b0152f15013aea695893ec40bfe1de15
Jonathan Binette <binettexyz@proton.me>
2025-04-17 23:39:26 -0400

    Lib: add instruction

--- lib/hex2rgba.nix
+++ lib/hex2rgba.nix
@@ -2,6 +2,10 @@
 {
   # Function for .css that expect an alpha value from 0 - 1
   # Inputs: hexColor is a string like "#RRGGBB", opacity is a float between 0 and 1
+  # use it like this in a string:
+  # hexToRgba = import ../../../../lib/hex2rgba.nix { inherit lib; };
+  # ${hexToRgba { hexColor = #000000; opacity = 0.96; }};
+
   hexColor,
   opacity,
 }:
