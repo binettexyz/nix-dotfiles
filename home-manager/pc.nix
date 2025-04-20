400a330233af5f95d6a52fe692b0e70d516c73a5
Jonathan Binette <binettexyz@proton.me>
2025-04-19 21:09:18 -0400

    Home-Manager: Edit XDG config

--- home-manager/pc.nix
+++ home-manager/pc.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
 
   services.udiskie.enable = true;
   services.udiskie.tray = "never";
+
+  # Set default directories without CAPS
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "/home/${config.home.username}/desktop";
+    documents = "/home/${config.home.username}/documents";
+    download = "/home/${config.home.username}/download";
+    pictures = "/home/${config.home.username}/pictures";
+    videos = "/home/${config.home.username}/videos";
+  };
 
   # Some applications like to overwrite this file, so let's just force it
   xdg.configFile."mimeapps.list".force = true;
@@ -15,8 +26,7 @@
       {
         "application/pdf" = [ "pdf.desktop" ];
         "application/postscript" = [ "pdf.desktop" ];
-        "application/rss+xml" = [ "rss.desktop" ];
-        "image/png" = [ browser ];
+        "image/png" = [ "img.desktop" ];
         "image/jpeg" = [ "img.desktop" ];
         "image/gif" = [ "img.desktop" ];
         "inode/directory" = [ "file.desktop" ];
@@ -25,12 +35,34 @@
         "text/html" = [ "text.desktop" ];
         "video/x-matroska" = [ "video.desktop" ];
         "x-scheme-handler/magnet" = [ "torrent.desktop" ];
-        "x-scheme-handler/mailto" = [ "mail.desktop" ];
         "x-scheme-handler/http" = [ browser ];
         "x-scheme-handler/https " = [ browser ];
         "x-scheme-handler/about" = [ browser ];
         "x-scheme-handler/unknown" = [ browser ];
       };
+  };
+
+  xdg.desktopEntries = {
+    pdf = {
+      name = "PDF Reader";
+      type = "Application";
+      exec = "${pkgs.zathura}/bin/zathura %f";
+    };
+    text = {
+      name = "Text Editor";
+      type = "Application";
+      exec = "${pkgs.foot}/bin/foot -e ${pkgs.helix}/bin/hx %f";
+    };
+    img = {
+      name = "Image Viewer";
+      type = "Application";
+      exec = "${pkgs.vimiv-qt}/bin/vimiv %f";
+    };
+    video = {
+      name = "Video Player";
+      type = "Application";
+      exec = "mpv %f";
+    };
   };
 
 }
