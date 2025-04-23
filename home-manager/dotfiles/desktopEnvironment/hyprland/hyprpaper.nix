8073e26597f4eb9d17177dc712dcef23f991c0d9
Jonathan Binette <binettexyz@proton.me>
2025-04-22 20:57:50 -0400

    Home-Manager+Hyprpaper: Change path to wallpaper

--- home-manager/dotfiles/desktopEnvironment/hyprland/hyprpaper.nix
+++ home-manager/dotfiles/desktopEnvironment/hyprland/hyprpaper.nix
@@ -1,11 +1,9 @@
 {
-
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "/home/binette/Pictures/wallpapers/gruvbox/004.jpg" ];
-      wallpaper = [ "HDMI-A-1,/home/binette/Pictures/wallpapers/gruvbox/004.jpg" ];
+      preload = ["/home/binette/pictures/wallpapers/gruvbox/004.jpg"];
+      wallpaper = ["HDMI-A-1,/home/binette/pictures/wallpapers/gruvbox/004.jpg"];
     };
   };
-
 }
