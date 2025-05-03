b9d65e1ad327ca381777b182d0ad2665dbbe63c8
Jonathan Binette <binettexyz@proton.me>
2025-05-03 09:27:33 -0400

    Overlays+sysact: Add logout entry

--- overlays/scripts/sysact.nix
+++ overlays/scripts/sysact.nix
@@ -1,16 +1,15 @@
-{ writeShellApplication }:
-
+{writeShellApplication}:
 writeShellApplication {
   name = "sysact";
   text = ''
     #!/run/current-system/sw/bin/sh
     if ! pgrep wofi > /dev/null; then
-      case "$(printf " Hibernate\n Sleep\n Reboot\nLock\n Shutdown" | wofi --dmenu -W 300 -H 200 -i  -p 'Action: ')" in
-    	  ' Hibernate') hyprlock & systemctl hibernate ;;
+      case "$(printf " Sleep\n Reboot\n Lock\n Shutdown\n Logout" | wofi --dmenu -W 300 -H 200 -i  -p 'Action: ')" in
     	  ' Sleep') systemctl suspend && hyprlock ;;
     	  ' Reboot') systemctl reboot ;;
-    	  'Lock') hyprlock;;
-    	  ' Shutdown')systemctl poweroff ;;
+    	  ' Lock') hyprlock;;
+    	  ' Shutdown') systemctl poweroff ;;
+        ' Logout') uwsm stop ;;
     	  *) exit 1 ;;
       esac
     fi
