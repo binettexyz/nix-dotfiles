b31698b0176cb0f5a297900dabae028526fc6ca3
Jonathan Binette <binettexyz@proton.me>
2025-05-03 23:28:46 -0400

    home-Manager: Prevent default diretory to be created

--- home-manager/pc.nix
+++ home-manager/pc.nix
@@ -1,13 +1,12 @@
-{ config, pkgs, ... }:
 {
-
-  services.udiskie.enable = true;
-  services.udiskie.tray = "never";
-
+  config,
+  pkgs,
+  ...
+}: {
   # Set default directories without CAPS
   xdg.userDirs = {
     enable = true;
-    createDirectories = true;
+    createDirectories = false;
     desktop = "/home/${config.home.username}/desktop";
     documents = "/home/${config.home.username}/documents";
     download = "/home/${config.home.username}/download";
@@ -19,27 +18,25 @@
   xdg.configFile."mimeapps.list".force = true;
   xdg.mimeApps = {
     enable = true;
-    defaultApplications =
-      let
-        browser = "librewolf.desktop";
-      in
-      {
-        "application/pdf" = [ "pdf.desktop" ];
-        "application/postscript" = [ "pdf.desktop" ];
-        "image/png" = [ "img.desktop" ];
-        "image/jpeg" = [ "img.desktop" ];
-        "image/gif" = [ "img.desktop" ];
-        "inode/directory" = [ "file.desktop" ];
-        "text/x-shellscript" = [ "text.desktop" ];
-        "text/plain" = [ "text.desktop" ];
-        "text/html" = [ "text.desktop" ];
-        "video/x-matroska" = [ "video.desktop" ];
-        "x-scheme-handler/magnet" = [ "torrent.desktop" ];
-        "x-scheme-handler/http" = [ browser ];
-        "x-scheme-handler/https " = [ browser ];
-        "x-scheme-handler/about" = [ browser ];
-        "x-scheme-handler/unknown" = [ browser ];
-      };
+    defaultApplications = let
+      browser = "librewolf.desktop";
+    in {
+      "application/pdf" = ["pdf.desktop"];
+      "application/postscript" = ["pdf.desktop"];
+      "image/png" = ["img.desktop"];
+      "image/jpeg" = ["img.desktop"];
+      "image/gif" = ["img.desktop"];
+      "inode/directory" = ["file.desktop"];
+      "text/x-shellscript" = ["text.desktop"];
+      "text/plain" = ["text.desktop"];
+      "text/html" = ["text.desktop"];
+      "video/x-matroska" = ["video.desktop"];
+      "x-scheme-handler/magnet" = ["torrent.desktop"];
+      "x-scheme-handler/http" = [browser];
+      "x-scheme-handler/https " = [browser];
+      "x-scheme-handler/about" = [browser];
+      "x-scheme-handler/unknown" = [browser];
+    };
   };
 
   xdg.desktopEntries = {
@@ -64,5 +61,4 @@
       exec = "mpv %f";
     };
   };
-
 }
