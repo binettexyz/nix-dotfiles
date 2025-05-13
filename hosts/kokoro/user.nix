8d6528d9b6693ee593d21a23e46d50540e95bac8
Jonathan Binette <binettexyz@proton.me>
2025-05-13 12:59:23 -0400

    Host+Home-Manager: Removed impermanence

--- hosts/kokoro/user.nix
+++ hosts/kokoro/user.nix
@@ -1,13 +1,11 @@
-{ flake, ... }:
-let
+{flake, ...}: let
   inherit (flake) inputs;
-in
-{
-
+in {
   imports = [
     ../../home-manager
     (inputs.impermanence + "/home-manager.nix")
     flake.inputs.nix-colors.homeManagerModule
+    flake.inputs.plasma-manager.homeManagerModules.plasma-manager
     #    flake.inputs.nixvim.homeManagerModules.nixvim
   ];
 
@@ -21,53 +19,4 @@ in
       lowSpec = true;
     };
   };
-
-  # ---Persistence---
-  home.persistence."/nix/persist/home/binette" = {
-    removePrefixDirectory = false;
-    allowOther = true;
-    directories = [
-      ".cache/BraveSoftware"
-      #".cache/Jellyfin Media Player"
-      ".cache/librewolf"
-      ".cache/qutebrowser"
-
-      #".config/BraveSoftware"
-      #".config/jellyfin.org"
-      ".config/qtile"
-      ".config/shell"
-      ".config/sops"
-
-      ".local/bin"
-      ".local/share/applications"
-      ".local/share/cargo"
-      ".local/share/gnupg"
-      #".local/share/jellyfinmediaplayer"
-      #".local/share/Jellyfin Media Player"
-      ".local/share/password-store"
-      ".local/share/Ripcord"
-      ".local/share/xorg"
-      ".local/share/zoxide"
-      ".local/share/qutebrowser"
-
-      ".git"
-      ".librewolf"
-      ".ssh"
-      ".gnupg"
-      ".zplug"
-
-      "documents"
-      "pictures"
-      "videos"
-      "downloads"
-    ];
-    files = [
-      ".config/greenclip.toml"
-      ".config/wall.png"
-      #".config/zoomus.conf"
-      ".local/share/history"
-      ".cache/greenclip.history"
-    ];
-  };
-
 }
