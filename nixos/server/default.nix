c03bbf483b4fa8c36028c781b25c1c5a0b56dac2
Jonathan Binette <binettexyz@proton.me>
2025-05-13 20:46:31 -0400

    System+Server: Removed Unnecessary stuff

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -4,9 +4,6 @@
   deviceType,
   ...
 }:
-let
-  name = "nixbuilder";
-in
 {
 
   imports = [ ./containers ];
@@ -16,10 +13,10 @@ in
     # ---Network File System---
     services.nfs.server = {
       enable = true;
-      exports = ''
-        /media 100.91.89.2(rw,insecure,no_subtree_check)
-        /media 100.67.150.87(rw,insecure,no_subtree_check)
-      '';
+#      exports = ''
+#        /media 100.91.89.2(rw,insecure,no_subtree_check)
+#        /media 100.67.150.87(rw,insecure,no_subtree_check)
+#      '';
     };
 
     # ---Docker Container---
@@ -32,28 +29,6 @@ in
       };
       oci-containers.backend = "podman";
     };
-
-    # ---Builder User---
-    users = {
-      groups.${name} = { };
-      users.${name} = {
-        useDefaultShell = true;
-        #shell = "/run/current-system/sw/bin/nologin";
-        isNormalUser = true;
-        createHome = true;
-        home = "/home/${name}";
-        group = "${name}";
-        extraGroups = [ "${name}" ];
-      };
-    };
-    security.doas.extraRules = [
-      {
-        users = [ "nixbuilder" ];
-        noPass = true;
-        cmd = "nixos-rebuild";
-      }
-    ];
-    nix.settings.trusted-users = [ "${name}" ];
-
   };
+
 }
