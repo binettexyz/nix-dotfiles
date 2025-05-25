542b682be287e8d50450b174262062398b0347e6
Jonathan Binette <binettexyz@proton.me>
2025-05-24 20:51:59 -0400

    Host+Hayate: Add colorScheme option

--- hosts/hayate/user.nix
+++ hosts/hayate/user.nix
@@ -9,13 +9,16 @@ in {
     #    flake.inputs.nixvim.homeManagerModules.nixvim
   ];
 
-  colorScheme = import ../../modules/colorSchemes/gruvbox-material.nix;
+  #colorScheme = import ../../modules/colorSchemes/gruvbox-material.nix;
 
   modules.hm = {
-    browser.librewolf.enable = true;
-    browser.qutebrowser.enable = true;
-    mpv = {
-      enable = true;
+    browser = {
+      librewolf.enable = true;
+      qutebrowser.enable = true;
+    };
+    mpv.enable = true;
+    theme = {
+      colorScheme = "gruvbox-material";
     };
   };
 }
