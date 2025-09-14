2f8820edb6b7dfa517d539899b681d59e95ea4ed
Jonathan Binette <binettexyz@proton.me>
2025-09-14 10:54:48 -0400

    Kageyami: Add internal ips

--- hosts/kageyami/config.nix
+++ hosts/kageyami/config.nix
@@ -1,21 +1,23 @@
-{
-  lib,
-  flake,
-  ...
-}: {
+{lib, ...}: {
   ## Custom modules ##
   modules = {
     bootloader.default = "rpi4";
     server.containers = {
+      actual-budget.enable = true;
       cloudflare-ddns.enable = true;
       gitea.enable = true;
-      home-assistant.enable = false;
+      home-assistant.enable = true;
       miniflux.enable = true;
       vaultwarden.enable = true;
     };
     system = {
       home.enable = true;
     };
+  };
+
+  device.network.ipv4 = {
+    internal = "192.168.2.17";
+    tailscale = "100.110.153.50";
   };
 
   services.syncthing.settings.folders = {
