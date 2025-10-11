35247f94df41768ac7ebc37ada6aa304a2015193
Jonathan Binette <binettexyz@proton.me>
2025-10-11 11:32:30 -0400

    Byakko+Laptop: Move fans control to laptop profile

--- hosts/byakko/config.nix
+++ hosts/byakko/config.nix
@@ -21,20 +21,4 @@
   device.videoOutput = ["eDP-1"];
   device.storage.ssd = true;
 
-  services.thinkfan = {
-    enable = true;
-    sensors = [ { type = "tpacpi"; query = "/proc/acpi/ibm/thermal"; indices = [ 0 1 2 ]; } ];
-    fans = [ { type = "tpacpi"; query = "/proc/acpi/ibm/fan"; } ];
-    levels = [
-      [0 0 55]
-      [1 55 60]
-      [2 60 63]
-      [3 63 66]
-      [4 66 70]
-      [5 70 72]
-      [6 72 75]
-      [7 75 80]
-      ["level auto" 80 255]
-    ];
-  };
 }
--- modules/profiles/laptop/default.nix
+++ modules/profiles/laptop/default.nix
@@ -58,6 +58,24 @@
         lidSwitchExternalPower = lib.mkDefault "lock";
       };
 
+      # Fans Control
+      thinkfan = {
+        enable = true;
+        sensors = [ { type = "tpacpi"; query = "/proc/acpi/ibm/thermal"; indices = [ 0 1 2  ]; } ];
+        fans = [ { type = "tpacpi"; query = "/proc/acpi/ibm/fan";  } ];
+        levels = [
+          [0 0 40]
+          [1 40 55]
+          [2 55 60]
+          [3 60 65]
+          [4 65 70]
+          [5 70 72]
+          [6 72 75]
+          [7 75 80]
+          ["level auto" 80 255]
+        ];
+      };
+
       # Reduce power consumption
       thermald.enable = true;
       tlp = {
