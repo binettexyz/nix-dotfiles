5d324d0db3287a30a1151b479d5b0c0ac8307fd4
Jonathan Binette <binettexyz@proton.me>
2026-03-02 06:14:40 -0500

    Syncthing: Fix typo

--- modules/programs/services/syncthing/syncthing.nix
+++ modules/programs/services/syncthing/syncthing.nix
@@ -1,40 +1,41 @@
-{ config, ... }:
 {
-  flake.nixosModules.syncthing = {
-    services.syncthing = {
-      enable = true;
-      group = config.home.username;
-      user = config.home.username;
-      configDir = "/home/${config.home.username}/.config/syncthing"; # Folder for Syncthing's settings and keys
-      dataDir = "/home/${config.home.username}/.local/sync";
-      overrideDevices = true; # overrides any devices added or deleted through the WebUI
-      overrideFolders = false; # overrides any folders added or deleted through the WebUI
-      settings = {
-        gui.theme = "black";
-        devices = {
-          "suzaku" = {
-            id = "QK7TWSU-CYQOBEW-MBBEVWM-X37XHVY-GSIBOCM-YHBDRJE-BNM2TIA-ZGW4MQX";
-            autoAcceptFolders = true;
-          };
-          "seiryu" = {
-            id = "VHONWML-AZNC73N-KBJ62KW-NEM27CZ-ZRDTB34-TUHPCYS-7X4B2HF-4L7NTQA";
-            autoAcceptFolders = true;
-          };
-          "genbu" = {
-            id = "3H6X5PB-BXBVXDM-FEI4VNL-EXEPUNM-6VW6TWO-273GWRH-4QA3EYS-OOETRQN";
-            autoAcceptFolders = false;
-          };
-          "byakko" = {
-            id = "YJDWZRL-XHRLD6X-Z5U5YG4-BQE44GI-PMU4RD4-LPNYCZF-4SUY5RY-2VMFQQK";
-            autoAcceptFolders = true;
+  flake.nixosModules.syncthing =
+    { config, ... }:
+    {
+      services.syncthing = {
+        enable = true;
+        group = config.meta.username;
+        user = config.meta.username;
+        configDir = "/home/${config.meta.username}/.config/syncthing"; # Folder for Syncthing's settings and keys
+        dataDir = "/home/${config.meta.username}/.local/sync";
+        overrideDevices = true; # overrides any devices added or deleted through the WebUI
+        overrideFolders = false; # overrides any folders added or deleted through the WebUI
+        settings = {
+          gui.theme = "black";
+          devices = {
+            "suzaku" = {
+              id = "QK7TWSU-CYQOBEW-MBBEVWM-X37XHVY-GSIBOCM-YHBDRJE-BNM2TIA-ZGW4MQX";
+              autoAcceptFolders = true;
+            };
+            "seiryu" = {
+              id = "VHONWML-AZNC73N-KBJ62KW-NEM27CZ-ZRDTB34-TUHPCYS-7X4B2HF-4L7NTQA";
+              autoAcceptFolders = true;
+            };
+            "ouryuu" = {
+              id = "TEGLOVR-2AL23J2-5C5XDT7-X3DKOIM-E5QBGA5-M6FVRUA-A5TEJ3J-P3VEUAQ";
+              autoAcceptFolders = false;
+            };
+            "byakko" = {
+              id = "55FSEXM-6SGGQXG-IUSWZ4X-Q7SVLY7-SU7UV3G-5XJUZRP-6I7KMQH-NB74IAF";
+              autoAcceptFolders = true;
+            };
           };
         };
+        #      gui = {
+        #        user = username;
+        #        password = "";
+        #      };
       };
-      #      gui = {
-      #        user = username;
-      #        password = "";
-      #      };
-    };
 
-  };
+    };
 }
