a893aa581f7fb1173a4723103f233fa7c69452ca
Jonathan Binette <binettexyz@proton.me>
2026-03-02 17:33:19 -0500

    Cleanup

--- modules/hosts/suzaku/config.nix
+++ modules/hosts/suzaku/config.nix
@@ -1,50 +1,48 @@
 { inputs, ... }:
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "suzaku";
-  flake.nixosModules.suzaku =
-    { lib, ... }:
-    {
-      imports = with inputs.self.nixosModules; [
-        binette
-        consoleGamingPreset
-        home-manager
-        impermanence
-        sunshine
-      ];
+  flake.nixosModules.suzaku = {
+    imports = with inputs.self.nixosModules; [
+      binette
+      consoleGamingPreset
+      home-manager
+      impermanence
+      #sunshine
+    ];
 
-      modules = {
-        bootloader = {
-          default = "grub";
-          asRemovable = false;
-          useOSProber = false;
-        };
-        desktopEnvironment = "plasma";
-        device = {
-          cpu = "amd";
-          hostname = "suzaku";
-          storage = {
-            hdd = true;
-            ssd = true;
-          };
-          type = "desktop";
-          tags = [
-            "workstation"
-            "console"
-            "gaming"
-            "highSpec"
-          ];
-          videoOutputs = [
-            "HDMI-A-1"
-            "HDMI-A-2"
-          ];
-        };
+    modules = {
+      bootloader = {
+        default = "grub";
+        asRemovable = false;
+        useOSProber = false;
       };
-
-      #services.flatpak.packages = [
-      #  "org.prismlauncher.PrismLauncher"
-      #  "com.heroicgameslauncher.hgl"
-      #  "net.retrodeck.retrodeck"
-      #  "dev.vencord.Vesktop"
-      #];
+      desktopEnvironment = "plasma";
+      device = {
+        cpu = "amd";
+        hostname = "suzaku";
+        storage = {
+          hdd = true;
+          ssd = true;
+        };
+        type = "desktop";
+        tags = [
+          "workstation"
+          "console"
+          "gaming"
+          "highSpec"
+        ];
+        videoOutputs = [
+          "HDMI-A-1"
+          "HDMI-A-2"
+        ];
+      };
     };
+
+    #services.flatpak.packages = [
+    #  "org.prismlauncher.PrismLauncher"
+    #  "com.heroicgameslauncher.hgl"
+    #  "net.retrodeck.retrodeck"
+    #  "dev.vencord.Vesktop"
+    #];
+  };
 }
