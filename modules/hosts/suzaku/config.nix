c85a698a87fe4182842b6feb6b761a3807bba406
Jonathan Binette <binettexyz@proton.me>
2026-04-25 13:20:31 -0400

    Suzaku: Disable wake on USB, changed power button behavior

--- modules/hosts/suzaku/config.nix
+++ modules/hosts/suzaku/config.nix
@@ -1,47 +1,62 @@
 { inputs, ... }:
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "suzaku";
-  flake.nixosModules.suzaku = {
-    imports = with inputs.self.nixosModules; [
-      binette
-      desktopGamingPreset
-      home-manager
-      impermanence
-      sunshine
-    ];
+  flake.nixosModules.suzaku =
+    { lib, ... }:
+    {
+      imports = with inputs.self.nixosModules; [
+        binette
+        desktopGamingPreset
+        home-manager
+        impermanence
+        sunshine
+      ];
 
-    modules = {
-      bootloader = {
-        default = "grub";
-        asRemovable = false;
-        useOSProber = false;
-      };
-      desktopEnvironment = "plasma";
-      device = {
-        cpu = "amd";
-        hostname = "suzaku";
-        storage = {
-          hdd = true;
-          ssd = true;
+      modules = {
+        bootloader = {
+          default = "grub";
+          asRemovable = false;
+          useOSProber = false;
         };
-        type = "desktop";
-        tags = [
-          "workstation"
-          "gaming"
-          "highSpec"
-        ];
-        videoOutputs = [
-          "HDMI-A-1"
-          "HDMI-A-2"
-        ];
+        desktopEnvironment = "hyprland";
+        device = {
+          cpu = "amd";
+          hostname = "suzaku";
+          storage = {
+            hdd = true;
+            ssd = true;
+          };
+          type = "desktop";
+          tags = [
+            "workstation"
+            "gaming"
+            "highSpec"
+          ];
+          videoOutputs = [
+            "HDMI-A-1"
+            "HDMI-A-2"
+          ];
+        };
       };
-    };
 
-    #services.flatpak.packages = [
-    #  "org.prismlauncher.PrismLauncher"
-    #  "com.heroicgameslauncher.hgl"
-    #  "net.retrodeck.retrodeck"
-    #  "dev.vencord.Vesktop"
-    #];
-  };
+      services.logind.settings.Login = {
+        HandlePowerKey = lib.mkForce "suspend";
+      };
+
+      services.udev.extraRules = ''
+        # Keychron keyboard acces from webapp
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0c10", MODE="0660", GROUP="binette", TAG+="uaccess", TAG+="udev-acl"
+        # Disable wake on USB
+        ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled", ATTR{driver/3-3/power/wakeup}="disabled"
+        ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled", ATTR{driver/1-1/power/wakeup}="disabled"
+        ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0c10", ATTR{power/wakeup}="disabled", ATTR{driver/1-2/power/wakeup}="disabled"
+      '';
+
+      #services.flatpak.packages = [
+      #  "org.prismlauncher.PrismLauncher"
+      #  "com.heroicgameslauncher.hgl"
+      #  "net.retrodeck.retrodeck"
+      #  "dev.vencord.Vesktop"
+      #];
+    };
 }
