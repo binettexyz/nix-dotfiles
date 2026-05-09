e7056dcb980e1fd6dd722a0ae5cd7bcbe9a2177a
Jonathan Binette <binettexyz@proton.me>
2026-05-09 09:42:27 -0400

    Suzaku: Add jovian's config, set as console and more

--- modules/hosts/suzaku/config.nix
+++ modules/hosts/suzaku/config.nix
@@ -7,6 +7,7 @@
       imports = with inputs.self.nixosModules; [
         binette
         desktopGamingPreset
+        jovian
         home-manager
         impermanence
         sunshine
@@ -28,24 +29,23 @@
           };
           type = "desktop";
           tags = [
+            "console"
             "workstation"
             "gaming"
             "highSpec"
           ];
           videoOutputs = [
-            "HDMI-A-1"
+            "DP-1"
             "HDMI-A-2"
           ];
         };
       };
 
-      services.logind.settings.Login = {
-        HandlePowerKey = lib.mkForce "suspend";
-      };
+      #services.logind.settings.Login = {
+      #  HandlePowerKey = lib.mkForce "suspend";
+      #};
 
       services.udev.extraRules = ''
-        # Keychron keyboard acces from webapp
-        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0c10", MODE="0660", GROUP="binette", TAG+="uaccess", TAG+="udev-acl"
         # Disable wake on USB
         ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled", ATTR{driver/3-3/power/wakeup}="disabled"
         ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled", ATTR{driver/1-1/power/wakeup}="disabled"
