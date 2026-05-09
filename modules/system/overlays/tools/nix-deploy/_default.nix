7f4f867f0df3d46ce63655952f3ca4d80e39f7e5
Jonathan Binette <binettexyz@proton.me>
2026-05-09 10:04:40 -0400

    Overlays: Add flake path

--- modules/system/overlays/tools/nix-deploy/_default.nix
+++ modules/system/overlays/tools/nix-deploy/_default.nix
@@ -3,7 +3,7 @@ writeShellApplication {
   name = "nix-deploy";
   text = ''
     pushd ~/.config/nixos
-    nixos-rebuild "$1" --flake .#"$2" --target-host "$2" --build-host "''${3:-$2}" --sudo
+    nixos-rebuild "$1" --flake ~/.config/nixos#"$2" --target-host "$2" --build-host "''${3:-$2}" --sudo
     popd
   '';
 }
--- modules/system/overlays/tools/nix-rebuild/_default.nix
+++ modules/system/overlays/tools/nix-rebuild/_default.nix
@@ -3,7 +3,7 @@ writeShellApplication {
   name = "nix-rebuild";
   text = ''
     pushd ~/.config/nixos
-    unbuffer nixos-rebuild "$@" --flake .# --sudo |& nom
+    unbuffer nixos-rebuild "$@" --flake ~/.config/nixos# --sudo |& nom
     popd
   '';
 }
