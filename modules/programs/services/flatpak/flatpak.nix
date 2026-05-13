20d0ebf06b619a133c16afc48f693fc7310abb3d
Jonathan Binette <binettexyz@proton.me>
2026-05-12 23:03:30 -0400

    Flatpak: Add session variables

--- modules/programs/services/flatpak/flatpak.nix
+++ modules/programs/services/flatpak/flatpak.nix
@@ -7,5 +7,11 @@
       services.flatpak.enable = true;
       xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
       xdg.portal.config.common.default = "gtk";
+      environment.sessionVariables = {
+        XDG_DATA_DIRS = [
+          "/var/lib/flatpak/exports/share"
+          "$HOME/.local/share/flatpak/exports/share"
+        ];
+      };
     };
 }
