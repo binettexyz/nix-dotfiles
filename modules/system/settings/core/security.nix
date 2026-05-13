634fe8b78e9ece2c2b3047dc6b00b4ed1eeb0c21
Jonathan Binette <binettexyz@proton.me>
2026-05-12 23:01:54 -0400

    Security: Removed blacklisted kernel modules

--- modules/system/settings/core/security.nix
+++ modules/system/settings/core/security.nix
@@ -38,40 +38,5 @@
       networking.extraHosts = hostsContent;
 
       environment.systemPackages = [ pkgs.doas-sudo-shim ];
-
-      boot.blacklistedKernelModules = [
-        # Obscure network protocols
-        "ax25"
-        "netrom"
-        "rose"
-        # Obscure/Legacy Filesystems
-        "adfs"
-        "affs"
-        "bfs"
-        "befs"
-        "efs"
-        "erofs"
-        "exofs"
-        "freevxfs"
-        "f2fs"
-        "vivid"
-        "gfs2"
-        "cramfs"
-        "jffs2"
-        "hfs"
-        "hfsplus"
-        "hpfs"
-        "jfs"
-        "minix"
-        "nilfs2"
-        "omfs"
-        "qnx4"
-        "qnx6"
-        "sysv"
-        "ufs"
-        "ksmbd" # SMB server module
-        "cifs" # Windows share
-        "uvcvideo" # Webcam
-      ];
     };
 }
