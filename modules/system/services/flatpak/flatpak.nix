1a9e520a7d160dc7a390833ef6bbffa4ccc3bdcc
Jonathan Binette <binettexyz@proton.me>
2026-06-26 20:13:06 -0400

    Flatpak: ...

--- modules/system/services/flatpak/flatpak.nix
+++ modules/system/services/flatpak/flatpak.nix
@@ -1,12 +1,12 @@
 { inputs, ... }:
 {
   flake.nixosModules.flatpak =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
     {
       imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
       services.flatpak.enable = true;
       xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-      xdg.portal.config.common.default = "gtk";
+      xdg.portal.config.common.default = lib.mkDefault "gtk";
       environment.sessionVariables = {
         XDG_DATA_DIRS = [
           "/var/lib/flatpak/exports/share"
